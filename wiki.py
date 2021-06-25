50c296aa01f945a1fe9a6e77f18fab7e06c8beeb
JoePriestaff <85694527+JoePriestaff@users.noreply.github.com>
2021-06-25 11:45:47 +0100

    Add files via upload

---
+++ wiki.py
@@ -0,0 +1,58 @@
+# #set up
+# day1 = ["basic loop",
+# "basic print", 
+# "len", 
+# "find character", 
+# "upper",  
+# "lower",
+# "capatilize",
+#  "count", 
+#  "find",
+#  "replace", 
+#  "strip",
+#  "import random",
+#  "random uniform", 
+#  "import randint"]
+
+
+
+# #wiki
+
+response = ["basic loop",
+ "basic print", 
+ "len", 
+ "find character", 
+ "upper",  
+ "lower",
+ "capatilize",
+ "count", 
+ "find",
+ "replace", 
+ "strip",
+ "import random",
+ "random uniform", 
+ "import randint"]
+
+x = True
+
+
+while x:
+     response = input("Enter Day 1 code >>>           \n").lower()
+     if response == "basic loop":
+         print =("for i in range(10): print(i)")
+     elif response == "basic print":
+         print("print("")")
+     elif response == "len":
+         print("print (len("")")
+     elif response == "find character":
+         print("print(""[1])")
+     elif response == "upper":
+         print("print(.upper())")
+     elif response == "lower":
+         print("print(.lower())")
+     elif response == "capatalise":
+         print("print(.capatalise())")
+     elif response == "count":
+         print(".count(x)" "print(x)")
+     else: 
+         print("Existence is pain")
